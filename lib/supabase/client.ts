f0916e5d8a74b646760de059db6e4f2a78393e87
Hardik <101324919+HardikTalaviya04@users.noreply.github.com>
2026-08-10 23:23:51 +0530

    supabase key

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+ "https://yctrdgusiomurxxsaqkt.supabase.co",
+    "sb_publishable_W2hjcPUNcTSCKLUrrwjcqg_xiSA_415",
   )
 }
--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -9,8 +9,8 @@ export async function updateSession(request: NextRequest) {
   })
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    "https://yctrdgusiomurxxsaqkt.supabase.co",
+    "sb_publishable_W2hjcPUNcTSCKLUrrwjcqg_xiSA_415",
     {
       cookies: {
         getAll() {
